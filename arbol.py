dcbc9fe7a7d30358026fd46500890ae7080bd7c2
Ozuna <oppaozuna@gmail.com>
2016-09-14 11:58:06 -0500

    Update

--- arbol.py
+++ arbol.py
@@ -28,39 +28,83 @@ def buscaHijas(papa):
 		if nodo[x].nodoPadre==papa:
 			hijas.append(nodo[x].n)
 	return hijas
-def masPreguntas():
-	
+
+def mas_Preguntas(padre):
+	#global nodo
+	#papa=len(nodo)+1
+	#padre.mostrarNodo()
+	print "excelente continuemos con mas preguntas para cuando el usuario haya introducido"
+	print padre.pregunta,padre.respuesta
+	print "no lo olvides tenlas en cuenta"
+	pregunta = raw_input("Intruduzca la pregunta por favor\n>:")
+	respuesta = raw_input("Introduzcalas posibles respuestas para %s separadas por coma ejemplo si,no,tal vez \n>:" %(pregunta))
+	oneR=respuesta.split(',')
+	for x in range(len(oneR)):
+	#print '',len(nodo)+1,papa,[0],nodo[len(nodo)-1].pregunta,oneR[x],'j'
+		padre.nodosHijos.append(len(nodo)+1)
+		nodo.append(Nodo(len(nodo)+1,padre.n,[],pregunta,oneR[x],''))
+		masPreguntas=raw_input("muy bien ahora dime quieres introducir un resultado para cuando el usuaro haya introducido: %s con respuesta %s\n(Y/N):" %(nodo[len(nodo)-1].pregunta,nodo[len(nodo)-1].respuesta))
+		if masPreguntas=='Y':
+			resp=raw_input("excelente introduce el resultado\n>:")
+			nodo[len(nodo)-1].resultado=resp
+		if masPreguntas=='N':		
+			mas_Preguntas(nodo[len(nodo)-1])
+		
+
+def buscaResultado(respuesta):
+	#respuesta.mostrarNodo()
+	print "buscando..."
+	for x in range(len(nodo)):
+		if nodo[x].pregunta==respuesta.pregunta and nodo[x].respuesta==respuesta:
+			print "Resultado encontrado\n"
+			return nodo[x].resultado
+
+def txt_Nodos():
+	nod = open("Nodos.txt","w")
+	for x in range(len(nodo)):
+		nod.write('Nodo:	'+str(nodo[x].n))
+		nod.write('\n')
+		nod.write('Padre:	'+str(nodo[x].nodoPadre))
+		nod.write('\n')
+		nod.write('Hijos:	'+str(nodo[x].nodosHijos))
+		nod.write('\n')
+		nod.write('Pregunta:'+str(nodo[x].pregunta))
+		nod.write('\n')
+		nod.write('Respuesta:'+str(nodo[x].respuesta))
+		nod.write('\n')
+		nod.write('Resultado:'+str(nodo[x].resultado))
+		nod.write('\n')
+		nod.write ("===============================")
+		nod.write('\n')
+	nod.close()
 def main():
-	nodo=[]
-	global nodo
+	
+	#global nodo
 	papa=1
 	masPreguntas='N'
 	print "Arbol de Desicion"
-	nodo.append(Nodo(1,0,[],"pregunta","respuesta","resultado"))
 	#mostrarNodos()
 	
 
 	#while True:
 	#	try:
-	while masPreguntas=='N':
-		pregunta = raw_input("Introduzca una pregunta\n>:")
-		if len(nodo)==1:
-			nodo[0].pregunta=pregunta
-		respuesta = raw_input("Introduzcalas posibles respuestas para %s separadas por coma ejemplo si,no,tal vez \n>:" %(pregunta))
-		oneR=respuesta.split(',')
-		for x in range(len(oneR)):
-		#print '',len(nodo)+1,papa,[0],nodo[len(nodo)-1].pregunta,oneR[x],'j'
-			nodo[len(nodo)-1-x].nodosHijos.append(len(nodo)+1)
-			nodo.append(Nodo(len(nodo)+1,papa,[],nodo[len(nodo)-1].pregunta,oneR[x],''))	 
-			masPreguntas=raw_input("muy bien ahora dime quieres introducir un resultado para cuando el usuaro haya introducido: %s con respuesta %s\n(Y/N):" %(nodo[len(nodo)-1].pregunta,nodo[len(nodo)-1].respuesta))
-			if masPreguntas=='Y':
-				resp=raw_input("excelente introduce el resultado\n>:")
-				nodo[len(nodo)-1].resultado=resp
-			if masPreguntas=='N':
-				papa=len(nodo)+1
-				print "excelente continuemos con mas preguntas para cuando el usuario haya introducido"
-				print nodo[len(nodo)-1].pregunta,nodo[len(nodo)-1].respuesta
-				print "no lo olvides tomolas en cuenta"
+	#while True:
+	pregunta = raw_input("Introduzca una pregunta\n>:")
+	if len(nodo)==1:
+		nodo[0].pregunta=pregunta
+	respuesta = raw_input("Introduzcalas posibles respuestas para %s separadas por coma ejemplo si,no,tal vez \n>:" %(pregunta))
+	oneR=respuesta.split(',')
+	for x in range(len(oneR)):
+	#print '',len(nodo)+1,papa,[0],nodo[len(nodo)-1].pregunta,oneR[x],'j'
+		nodo[len(nodo)-1-x].nodosHijos.append(len(nodo)+1)
+		nodo.append(Nodo(len(nodo)+1,papa,[],nodo[len(nodo)-1].pregunta,oneR[x],'NULL'))	 
+		masPreguntas=raw_input("muy bien ahora dime quieres introducir un resultado para cuando el usuaro haya introducido: %s con respuesta %s\n(Y/N):" %(nodo[len(nodo)-1].pregunta,nodo[len(nodo)-1].respuesta))
+		if masPreguntas=='Y':
+			resp=raw_input("excelente introduce el resultado\n>:")
+			nodo[len(nodo)-1].resultado=resp
+		if masPreguntas=='N':
+			#nodo[len(nodo)-1].mostrarNodo()		
+			mas_Preguntas(nodo[len(nodo)-1])
 
 	print "excelente ya he aprendido manos a la obra"
 	print nodo[0].pregunta
@@ -70,7 +114,10 @@ def main():
 		print nodo[hijas[x]-1].respuesta
 	print "escriba una\n"
 	Respuesta=raw_input()
-
+	#resultadoFinal=buscaResultado(Respuesta)
+	#print resultadoFinal
+	txt_Nodos()
+	
 
 
 
@@ -82,5 +129,8 @@ def main():
 	#		print ("error")
 
 
+
+nodo=[]
 if __name__=="__main__":
+	nodo.append(Nodo(1,0,[],"pregunta","respuesta","resultado"))
 	main()
